f9e7fb2268a032f27fc5f4e657e7bd6499ca0364
Kevin Dungs <kevin@dun.gs>
2023-04-14 17:29:35 +0200

    Add TODO to applicative.

--- applicative.go
+++ applicative.go
@@ -3,6 +3,7 @@ package result
 // Apply.
 // TODO(kdungs): Documentation for `Apply`.
 // apply :: R (a -> b) -> R a -> R b
+// TODO(kdungs): Reconsider implementation of `Apply` as it's not truly lazy.
 func Apply[A any, B any](r R[func(A) B]) func(R[A]) R[B] {
 	f, err := r.Unwrap()
 	if err != nil {
