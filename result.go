5870f3622460eb7e948c0c526c57eccc4c0441ff
Kevin Dungs <kevin@dun.gs>
2023-04-19 16:11:07 +0200

    Switch to interface-based implementation.

--- result.go
+++ result.go
@@ -9,41 +9,59 @@
 package result
 
 // R is a generic result that can either hold a value of type T or an error.
-type R[T any] struct {
+type R[T any] interface {
+	// Unwrap returns both the value and the error component of `r`. Only one
+	// of them will hold a meaningful value as it is custom for Go functions
+	// that return a value and an error. Calling code should check `err !=
+	// nil`.
+	Unwrap() (T, error)
+
+	// Or returns the value part of `r` or the provided default value `d` in
+	// case `r` is actually holding an error.
+	Or(T) T
+}
+
+type rErr[T any] struct {
 	err error
-	v   T
 }
 
-// Unwrap returns both the value and the error component of `r`. Only one of
-// them will hold a meaningful value as it is custom for Go functions that
-// return a value and an error. Calling code should check `err != nil`.
-func (r R[T]) Unwrap() (T, error) {
-	return r.v, r.err
+func (r rErr[T]) Unwrap() (T, error) {
+	return *new(T), r.err
 }
 
-// Or returns the value part of `r` or the provided default value `d` in case
-// `r` is actually holding an error.
-func (r R[T]) Or(d T) T {
-	if r.err != nil {
-		return d
-	}
+func (r rErr[T]) Or(d T) T {
+	return d
+}
+
+type rVal[T any] struct {
+	v T
+}
+
+func (r rVal[T]) Unwrap() (T, error) {
+	return r.v, nil
+}
+
+func (r rVal[T]) Or(_ T) T {
 	return r.v
-}
-
-// Wrap takes a value and an error (e.g. from a function returning those two)
-// and turns them into an `R[T]`.
-func Wrap[T any](v T, err error) R[T] {
-	return R[T]{err, v}
 }
 
 // Of constructs an `R[T]` from a value of type `T`.
 func Of[T any](v T) R[T] {
-	return Wrap(v, nil)
+	return rVal[T]{v}
 }
 
 // OfErr[T] constructs an `R[T]` holding an error.
 // Since `T` cannot be inferred from the error alone, calling code is required
 // to provide it, e.g. `OfErr[int](errors.New("not a positive number"))`.
 func OfErr[T any](err error) R[T] {
-	return Wrap(*new(T), err)
+	return rErr[T]{err}
+}
+
+// Wrap takes a value and an error (e.g. from a function returning those two)
+// and turns them into an `R[T]`.
+func Wrap[T any](v T, err error) R[T] {
+	if err != nil {
+		return OfErr[T](err)
+	}
+	return Of(v)
 }
